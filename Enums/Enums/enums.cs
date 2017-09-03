06d5c2fde17be38e2e4140d1ef0f7041d7d93f4f
David Duym <david.duym1@telenet.be>
2017-09-03 22:46:04 +0200

    2

--- Enums/Enums/enums.cs
+++ Enums/Enums/enums.cs
@@ -2,11 +2,26 @@
 
 namespace Enums
 {
-    class Program
+    public enum ShippingMethod
+    {
+        regularAirmail = 1,
+        registeredAirlmail = 2,
+        express = 3
+    }
+    class enums
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var method = ShippingMethod.express;
+            Console.WriteLine((int)method);
+
+            var methodID = 3;
+            Console.WriteLine((ShippingMethod)methodID);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethods = (ShippingMethod) Enum.Parse(typeof (ShippingMethod), methodName);
         }
     }
 }
