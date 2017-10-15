c0c4be31c4948bbc6ee7c1bbfa8102897d3c3062
David Duym <david.duym1@telenet.be>
2017-10-15 08:10:11 +0200

    Roman converter

---
+++ Romijnsconverter/Romijnsconverter/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Romijnsconverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.btnC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblresult
+            // 
+            this.lblresult.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresult.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblresult.Location = new System.Drawing.Point(21, 162);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(194, 50);
+            this.lblresult.TabIndex = 1;
+            this.lblresult.Text = "label1";
+            this.lblresult.Click += new System.EventHandler(this.lblresult_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(27, 110);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(75, 23);
+            this.btnC.TabIndex = 2;
+            this.btnC.Text = "Converteer";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 221);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Romijnse Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblresult;
+        private System.Windows.Forms.Button btnC;
+    }
+}
+
---
+++ Romijnsconverter/Romijnsconverter/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Romijnsconverter
+{
+    public partial class Form1 : Form
+    {
+        int[] index = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+        string[] rIndex = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+        int indexLimit = 12;
+        string output = "";
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void lblresult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+               
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            output = "";
+            num = Convert.ToInt32(lblresult.Text);
+            while (num > 0)
+            {
+                num = find(num);
+
+            }
+            lblresult.Text = output;
+        }
+        public int find(int Num)
+        {
+            int i = 0;
+            
+            while (index(i) <= Num)
+            {
+                i += 1;
+            }
+            
+            if (i != 0)
+            {
+                indexLimit = i - 1;
+            }
+            else
+            {
+                indexLimit = 0;
+            }
+            output = output + rIndex(indexlimit);
+            
+            Num = Num - index(indexLimit);
+            // hier ben ik dus op aan het dutsen, in de while loop leest hij mij array uit maar dan in de output van de functie wil hij deze niet herkennen,
+            //gebruik ik nu de function fout, of zit er elders een probleem?
+            return Num;
+            
+        }
+    }
+}
